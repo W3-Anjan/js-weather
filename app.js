8cb7019ed5530964d958fc1ea271ec4cbf08b853
Anjan Debnath <anjan@w3engineers.com>
2022-03-14 18:28:39 +0600

    javascript basic functionality added

---
+++ app.js
@@ -0,0 +1,109 @@
+// SELECT ELEMENTS
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p")
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+const notificationElement = document.querySelector(".notification");
+
+// App data
+const weather = {
+
+    
+}
+
+weather.temperature = {
+    unit: "celsius"
+}
+
+// APP CONSTS AND VARS
+const KELVIN = 273;
+// API KEY
+const key = "82005d27a116c2880c8f0fcb866998a0";
+
+
+
+if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+} else{
+    notificationElement.innerHTML = "Geolocation is not supported by this browser.";
+}
+
+
+function showPosition(position){
+
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    // notificationElement.style.display = "block";
+    // notificationElement.innerHTML = "Latitude: " + latitude +
+    // "<br>Longitude: " + longitude;
+
+    getWeather(latitude, longitude);
+}
+
+function showError(error){
+
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = `<p>${error.message}</p>`
+}
+
+function getWeather(latitude, longitude){
+
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+
+    //console.log(api);
+
+    fetch(api)
+    // The fetch() method returns a Promise. After the fetch() method, include the Promise method then():
+    .then(function(response){
+        // handle the response
+
+        let data = response.json();
+        return data; // its an object
+    })
+    .then(function(data){
+        weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+        weather.description = data.weather[0].description;
+        weather.iconId = data.weather[0].icon;
+        weather.city = data.name;
+        weather.country = data.sys.country;
+
+    })
+    .then(function(){
+        displayWeather();
+
+    });
+
+    function displayWeather(){
+        iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+        tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+        descElement.innerHTML = weather.description;
+        locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+
+    }
+}
+
+
+// C to F conversion
+function celsiusToFahrenheit(temperature){
+    return (temperature * 9/5) + 32;
+}
+
+tempElement.addEventListener("click", function(){
+
+    if(weather.temperature.unit === undefined) return;
+
+    if(weather.temperature.unit == "celsius"){
+
+        let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
+        fahrenheit = Math.floor(fahrenheit)
+
+        tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
+        weather.temperature.unit = "fahrenheit"
+
+    }else{
+        tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+        weather.temperature.unit = "celsius"
+    }
+    
+});
